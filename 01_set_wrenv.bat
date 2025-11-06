51f22e93ab55b217d059d8f6915411cba453e768
Rich Moore <rmoorewrs@gmail.com>
2025-11-06 09:51:18 -0500

    remove redundant set commands from 01_set_wrenv.bat

--- 01_set_wrenv.bat
+++ 01_set_wrenv.bat
@@ -3,17 +3,7 @@ rem Note: Please set VXWORKS_INSTALL_PATH to the Unix-style path equivalent of W
 set WIND_BASE=C:\vx\vx2509
 set VXWORKS_INSTALL_PATH=/c/vx/vx2509
 set VXWORKS_VERSION=25.09
-set PROJECT_NAME=rpi4
-set BSP_NAME=rpi_4_0_1_3_0
-set DTS_FILE=rpi-4b.dts
 
-rem set this section for your target's IP setup
-set TARGET_IP=192.168.12.35
-set SERVER_IP=192.168.12.51
-set GATEWAY_IP=192.168.12.1
-set NETMASK=255.255.255.0
-set NETMASKHEX=ffffff00
-set NETMASKCIDR=24
 
 echo setting VxWorks environment and starting bash on Windows
 echo next run "./02_create_rpi4.sh" to build your project
